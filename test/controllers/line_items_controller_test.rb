330eeb331bcae327a8fb07f2669c5d49b6391b1f
escray <zescray@gmail.com>
2020-03-27 11:00:10 +0800

    Your Cart

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -25,7 +25,10 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     follow_redirect!
 
     assert_select 'h2', 'Your Pragmatic Cart'
-    assert_select 'li', 'Programming Ruby 1.9'
+    # TODO: assert_select option
+    assert_select 'main > ul > li', "1
+      \u00D7
+      Programming Ruby 1.9"
   end
 
   test 'should show line_item' do
@@ -39,7 +42,12 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should update line_item' do
-    patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+    patch line_item_url(@line_item),
+          params: {
+            line_item: {
+              product_id: @line_item.product_id
+            }
+          }
     assert_redirected_to line_item_url(@line_item)
   end
 
