bc79860acd81ce623cc7c27a32ebd05eb649df3f
escray <zescray@gmail.com>
2020-04-07 10:44:35 +0800

    Iteration J2: Authenticating Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  get 'sessions/new'
+
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :users
   resources :orders
   resources :line_items
