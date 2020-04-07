67a43ba55de1468b3098eb2b6ef474e4bfa67bb1
escray <zescray@gmail.com>
2020-04-07 15:43:40 +0800

    J4: Adding a Sidebar, More Administration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  after_destroy :ensure_an_admin_remains
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+  class Error < StandardError
+  end
+
+  private
+
+  def ensure_an_admin_remains
+    raise Error, "Can't delete last user" if User.count.zero?
+  end
 end
